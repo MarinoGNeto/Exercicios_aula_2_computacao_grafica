4352907451cfad3b591a67770a42a99caf035731
Yuri Martins <60901698+yuridmd26@users.noreply.github.com>
2024-03-03 15:44:30 -0300

    Colisao de particulas
    Criado um método para fazer a implementação do teste unitário.

--- src/main/java/br/unisociesc/ColisaoParticulas.java
+++ src/main/java/br/unisociesc/ColisaoParticulas.java
@@ -8,34 +8,37 @@ public class ColisaoParticulas
 	{
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.print("Informe a massa da partícula 1 (em Kg): ");
+		System.out.print("Informe a massa da partícula 1 (em kg): ");
 		double dMassaPart1 = scanner.nextDouble();
-		System.out.print("Informe a velocidade inicial da partícula 1 (Linha x) em m/s: ");
+		System.out.print("Informe a velocidade inicial da partícula 1 (Linha X) em m/s: ");
 		double dVelocidadeX1 = scanner.nextDouble();
-		System.out.print("Informe a velocidade inicial da partícula 1 (Linha y) em m/s: ");
+		System.out.print("Informe a velocidade inicial da partícula 1 (Linha Y) em m/s: ");
 		double dVelocidadeY1 = scanner.nextDouble();
 
-		System.out.print("Informe a massa da partícula 2 (em Kg): ");
+		System.out.print("Informe a massa da partícula 2 (em kg): ");
 		double dMassaPart2 = scanner.nextDouble();
-		System.out.print("Informe a velocidade inicial da partícula 2 (Linha x) em m/s: ");
+		System.out.print("Informe a velocidade inicial da partícula 2 (Linha X) em m/s: ");
 		double dVelocidadeX2 = scanner.nextDouble();
-		System.out.print("Informe a velocidade inicial da partícula 2 (Linha y) em m/s: ");
+		System.out.print("Informe a velocidade inicial da partícula 2 (Linha Y) em m/s: ");
 		double dVelocidadeY2 = scanner.nextDouble();
 
-		double[] velocidadesFinaisParticula1 = new double[2];
-		velocidadesFinaisParticula1[0] = ((dMassaPart1) * dVelocidadeX1 + 2 * dMassaPart2 * dVelocidadeX2) / (dMassaPart1 + dMassaPart2);
-		velocidadesFinaisParticula1[1] = ((dMassaPart1 - dMassaPart2) * dVelocidadeY1 + 2 * dMassaPart2 * dVelocidadeY2) / (dMassaPart1 + dMassaPart2);
-		
-		System.out.println("Velocidade final após impacto da partícula 1 (Linha x): " + velocidadesFinaisParticula1[0]);
-		System.out.println("Velocidade final após impacto da partícula 1 (Linha y): " + velocidadesFinaisParticula1[1]);
-		
-		double[] velocidadesFinaisParticula2 = new double[2];
-		velocidadesFinaisParticula2[0] = ((dMassaPart1) * dVelocidadeX1 + 2 * dMassaPart2 * dVelocidadeX2) / (dMassaPart1 + dMassaPart2);
-		velocidadesFinaisParticula2[1] = ((dMassaPart1 - dMassaPart2) * dVelocidadeY1 + 2 * dMassaPart2 * dVelocidadeY2) / (dMassaPart1 + dMassaPart2);
+		double[] adVelocidadesFinaisParticula1 = calcularVelocidadeFinal(dMassaPart1, dVelocidadeX1, dVelocidadeY1, dMassaPart2, dVelocidadeX2, dVelocidadeY2);
+		System.out.println("Velocidade final após impacto da partícula 1 (Linha X): " + adVelocidadesFinaisParticula1[0]);
+		System.out.println("Velocidade final após impacto da partícula 1 (Linha Y): " + adVelocidadesFinaisParticula1[1]);
 
-		System.out.println("Velocidade final após impacto da partícula 2 (Linha x): " + velocidadesFinaisParticula2[0]);
-		System.out.println("Velocidade final após impacto da partícula 2 (Linha y): " + velocidadesFinaisParticula2[1]);
+		double[] adVelocidadesFinaisParticula2 = calcularVelocidadeFinal(dMassaPart1, dVelocidadeX1, dVelocidadeY1, dMassaPart2, dVelocidadeX2, dVelocidadeY2);
+		System.out.println("Velocidade final após impacto da partícula 2 (Linha X): " + adVelocidadesFinaisParticula2[0]);
+		System.out.println("Velocidade final após impacto da partícula 2 (Linha Y): " + adVelocidadesFinaisParticula2[1]);
 
 		scanner.close();
+	}
+
+	public static double[] calcularVelocidadeFinal(double dMassaPart1, double dVelocidadeX1, double dVelocidadeY1, double dMassaPart2, double dVelocidadeX2, double dVelocidadeY2) 
+	{
+		double[] adVelocidadeFinal = new double[2];
+		
+		adVelocidadeFinal[0] = ((dMassaPart1) * dVelocidadeX1 + 2 * dMassaPart2 * dVelocidadeX2) / (dMassaPart1 + dMassaPart2);
+		adVelocidadeFinal[1] = ((dMassaPart1 - dMassaPart2) * dVelocidadeY1 + 2 * dMassaPart2 * dVelocidadeY2) / (dMassaPart1 + dMassaPart2);
+		return adVelocidadeFinal;
 	}
 }
