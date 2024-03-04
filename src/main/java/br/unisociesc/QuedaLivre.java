71d31fa200c913192fc237676ecabc8be4099eb8
Yuri Martins <60901698+yuridmd26@users.noreply.github.com>
2024-03-03 21:53:37 -0300

    Criado teste unitario de Queda Livre
    Criado teste unitario do exercicio de queda livre.

--- src/main/java/br/unisociesc/QuedaLivre.java
+++ src/main/java/br/unisociesc/QuedaLivre.java
@@ -1,12 +1,44 @@
 package br.unisociesc;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class QuedaLivre {
+	
+	final static double ACELERACAO_GRAVIDADE = 9.8;
+	
+	public static class DadosQueda
+	{
+		 private final double dTempo;
+		 private final double dAltura;
+		 private final double dVelocidade;
+		 
+		 public DadosQueda(double tempo, double altura, double velocidade) 
+		 {
+		        this.dTempo = tempo;
+		        this.dAltura = altura;
+		        this.dVelocidade = velocidade;
+		 }
+
+		 public double GetTempo() 
+		 {
+			 return dTempo;
+		 }
+
+		 public double GetAltura() 
+		 {
+			 return dAltura;
+		 }
+
+		 public double GetVelocidade() 
+		 {
+			 return dVelocidade;
+		 }
+	};
+	
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        final double ACELERACAO_GRAVIDADE = 9.8;
 
         System.out.print("Digite a altura inicial do objeto (metros): ");
         double dPosicaoInicial = scanner.nextDouble();
@@ -14,21 +46,54 @@ public class QuedaLivre {
         System.out.print("Digite o intervalo de tempo (segundos): ");
         double dIntervaloTempo = scanner.nextDouble();
 
-        double dTempo = 0;
-        double dPosicaoCalculada = dPosicaoInicial;
-        double dVelocidadeCalculada = 0;
-
-        System.out.println("Tempo (s)\tPosição (m)\tVelocidade (m/s)");
-
-        while (dPosicaoCalculada >= 0) {
-            dPosicaoCalculada = dPosicaoInicial - 0.5 * ACELERACAO_GRAVIDADE * Math.pow(dTempo, 2);
-            dVelocidadeCalculada = ACELERACAO_GRAVIDADE * dTempo;
-
-            System.out.printf("%.2f\t\t%.2f\t\t%.2f%n", dTempo, dPosicaoCalculada, dVelocidadeCalculada);
-
-            dTempo += dIntervaloTempo;
-        }
-
+        List<DadosQueda> listaDadosQueda = calculaExibePosicaoVelocidadeQueda(dPosicaoInicial, dIntervaloTempo);
+        exibeResultadosQuedaLivre(listaDadosQueda);
+        
         scanner.close();
+    }
+    
+    public static List<DadosQueda> calculaExibePosicaoVelocidadeQueda(double intervaloTempo, double alturaInicial) 
+    {
+    	 List<DadosQueda> listaDadosQueda = new ArrayList<>();
+    	
+        double dAlturaAtual = alturaInicial;
+        double dTempo = 0;
+        double dVelocidade = 0;
+
+        listaDadosQueda.add(new DadosQueda(dTempo, dAlturaAtual, dVelocidade));
+        
+        while (dAlturaAtual > 0) 
+        {
+        	dTempo += intervaloTempo;
+            double dProximaAltura = alturaInicial - ACELERACAO_GRAVIDADE * Math.pow(dTempo, 2) / 2;
+            dVelocidade =  ACELERACAO_GRAVIDADE * dTempo;
+
+            listaDadosQueda.add(new DadosQueda(dTempo, dProximaAltura, dVelocidade));
+            
+            if (dProximaAltura <= 0) 
+            {
+            	dTempo = Math.sqrt((2 * alturaInicial) / ACELERACAO_GRAVIDADE);
+            	dAlturaAtual = 0;
+            	dVelocidade = ACELERACAO_GRAVIDADE * dTempo;
+                
+            	listaDadosQueda.add(new DadosQueda(dTempo, dProximaAltura, dVelocidade));
+            } 
+            else 
+            {
+            	listaDadosQueda.add(new DadosQueda(dTempo, dProximaAltura, dVelocidade));
+            	 dAlturaAtual = dProximaAltura;
+            }
+        }
+        
+        return listaDadosQueda;
+    }
+    
+    private static void exibeResultadosQuedaLivre(List<DadosQueda> listaDadosQueda) 
+    {
+        System.out.println("Tempo (s)\tPosição (m)\tVelocidade (m/s)");
+        for (DadosQueda dadosQueda : listaDadosQueda) 
+        {
+        	System.out.printf("%.2f\t\t%.2f\t\t%.2f%n", dadosQueda.GetTempo(), dadosQueda.GetAltura(), dadosQueda.GetVelocidade());
+        }
     }
 }
--- src/test/java/br/unisociesc/QuedaLivreTest.java
+++ src/test/java/br/unisociesc/QuedaLivreTest.java
@@ -1,13 +1,38 @@
 package br.unisociesc;
 
 import junit.framework.TestCase;
+
+import java.util.List;
+
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class QuedaLivreTest {
 
-    @Test
-    public void shouldBeAbleToCalculateQuedaLivre() {
+	@Test
+    public void shouldBeAbleToCalculateQuedaLivre() 
+    {
+    	  double alturaInicial = 100;
+          double intervaloTempo = 1;
+          List<QuedaLivre.DadosQueda> listaDadosQueda = QuedaLivre.calculaExibePosicaoVelocidadeQueda(intervaloTempo, alturaInicial);
 
+          double[][] listaDadosEntrada = {
+              {0.0, 200.0, 0.0},
+              {1.0, 95.1, 9.81},
+              {2.0, 80.38, 19.62},
+              {3.0, 55.85, 29.43},
+              {4.0, 21.51, 39.24},
+              {4.51, 0.0, 44.29}
+          };
+
+          assertEquals(listaDadosEntrada.length, listaDadosQueda.size());
+
+          for (int i = 0; i < listaDadosEntrada.length; i++) 
+          {
+              assertEquals(listaDadosEntrada[i][0], listaDadosQueda.get(i).GetTempo());
+              assertEquals(listaDadosEntrada[i][1], listaDadosQueda.get(i).GetAltura());
+              assertEquals(listaDadosEntrada[i][2], listaDadosQueda.get(i).GetVelocidade());
+          }
     }
-
 }
