a2057c454f32f6d17687ad9f5f87b42a86c09522
MarinoGNeto <marino.goncalvesneto@gmail.com>
2024-03-03 21:00:50 -0300

    Adicionando classe de teste

---
+++ src/test/java/br/unisociesc/QuedaLivreTest.java
@@ -0,0 +1,13 @@
+package br.unisociesc;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class QuedaLivreTest {
+
+    @Test
+    public void shouldBeAbleToCalculateQuedaLivre() {
+
+    }
+
+}
