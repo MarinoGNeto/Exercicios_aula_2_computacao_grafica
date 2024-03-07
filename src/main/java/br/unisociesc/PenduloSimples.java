730feb8ca03509335fbf7dbd7b7c7214c7db6675
MarinoGNeto <marino.goncalvesneto@gmail.com>
2024-03-07 15:09:32 -0300

    Finalizando pendulo simples

--- src/main/java/br/unisociesc/PenduloSimples.java
+++ src/main/java/br/unisociesc/PenduloSimples.java
@@ -1,34 +1,44 @@
 package br.unisociesc;
 
+import java.util.Scanner;
+
 public class PenduloSimples {
 
+    /**
+     * Gravidade da Terra
+     */
     private static final double GRAVIDADE = 9.8;
 
-    // Método para calcular a posição angular do pêndulo em relação ao tempo
-    public static double calcularPosicaoAngular(double comprimento, double massa, double tempo) {
-        // Calculando o período do pêndulo
-        double periodo = 2 * Math.PI * Math.sqrt(comprimento / (GRAVIDADE * massa));
-
-        // Calculando o ângulo usando a fórmula do pêndulo
-        double angulo = Math.cos((2 * Math.PI * tempo) / periodo);
-
-        return angulo;
-    }
-
     public static void main(String[] args) {
-        // Parâmetros do pêndulo
-        double comprimento = 1.0; // comprimento da corda em metros
-        double massa = 1.0; // massa do objeto em kg
+        Scanner scanner = new Scanner(System.in);
 
-        // Simulação do movimento do pêndulo por 5 segundos
-        System.out.println("Simulação do Movimento do Pêndulo Simples por 5 segundos:");
-        System.out.println("Comprimento da corda: " + comprimento + " metros");
-        System.out.println("Massa do objeto: " + massa + " kg");
-        System.out.println("---------------------------------------------------------");
-        System.out.println("Tempo(s)     Ângulo(rad)");
-        for (double t = 0; t <= 5; t += 0.1) {
-            double angulo = calcularPosicaoAngular(comprimento, massa, t);
-            System.out.printf("%.2f          %.2f\n", t, angulo);
+        /**
+         * Parâmetros do pêndulo utilizados
+          */
+        System.out.print("Insira o comprimento da corda: ");
+        double comprimentoCorda = scanner.nextDouble();
+
+        System.out.print("Insira o intervalo de tempo: ");
+        double intervaloTempo = scanner.nextDouble();
+
+        System.out.print("Insira a massa do objeto: ");
+        double massa = scanner.nextDouble();
+
+        System.out.print("Insira o ângulo utilizado: ");
+        double anguloRadiano = Math.toRadians(scanner.nextDouble());
+
+        /**
+         * Calculo de período do pêndulo utilizando formula de pêndulo simples.
+         */
+        double periodo = 2 * Math.PI * Math.sqrt(comprimentoCorda / (GRAVIDADE * massa));
+
+        for (double tempo = 0; tempo <= intervaloTempo; tempo++) {
+            /**
+             * Calculo do ângulo
+             */
+            double angulo = anguloRadiano * Math.cos(Math.sqrt((GRAVIDADE * massa / comprimentoCorda)) * tempo);
+
+            System.out.printf("Para o tempo %.2f segundos, o ângulo do pêndulo é de %.2f graus.\n", tempo, Math.toDegrees(angulo));
         }
     }
 
